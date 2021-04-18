33afe962c0f58e620c5a572a3aadacb139e7c4be
Chris Williamson <chriswill0629@gmail.com>
2021-04-18 03:00:12 -0400

    fixing bugs with adding html from javascript

--- js/main.js
+++ js/main.js
@@ -9,51 +9,67 @@ function book(name, author, pages, read, bookNum) {
     //return { name, author, pages }
 }
 book.prototype.haveRead = function(){
-        console.log(this.name, this.author, this.pages);
+    if(this.read === "Have Not Read"){
+        this.read = "Have Read";
+        bookID = this.bookNum;
+        document.getElementById(bookID).textContent = this.read
+        
+    }else if(this.read === "Have Read"){
+        this.read = "Have Not Read"
+        bookID = this.bookNum;
+        document.getElementById(bookID).textContent = this.read
+    }
 }
 
 const b1Name =  "2001: a Space Odyssey";
 const b1Author = "Arthur C. Clark";
 const b1Pages = "296";
-const b1Read = "Have Read"
-const bookNum = "book0"
+const b1Read = "Have Not Read"
+const b1bookNum = "book0";
 
 const b2Name = "Magic of Recluse";
 const b2Author = "L.E. Modesitt, Jr";
 const b2Pages = "501";
-const b2Read = "Have Read";
+const b2Read = "Have Not Read";
+const b2bookNum = "book1";
 
 const b3Name = "The Eyes Of The Dragon";
 const b3Author = "Stephen King";
 const b3Pages = "384";
-const b3Read = "Have Read";
+const b3Read = "Have Not Read";
+const b3bookNum = "book2";
 
 const b4Name = "The Ruins of Gorlan";
 const b4Author = "John Flanagan";
 const b4Pages = "288";
-const b4Read = "Have Read";
+const b4Read = "Have Not Read";
+const b4bookNum = "book3";
 
 const b5Name = "Wizards First Rule";
 const b5Author = "Terry Goodkind";
 const b5Pages = "576";
-const b5Read = "Have Read";
+const b5Read = "Have Not Read";
+const b5bookNum = "book4";
 
-myLibrary.push(new book(b1Name, b1Author, b1Pages, b1Read, bookNum))
-myLibrary.push(new book(b2Name, b2Author, b2Pages, b2Read))
-myLibrary.push(new book(b3Name, b3Author, b3Pages, b3Read))
-myLibrary.push(new book(b4Name, b4Author, b4Pages, b4Read))
-myLibrary.push(new book(b5Name, b5Author, b5Pages, b5Read))
+myLibrary.push(new book(b1Name, b1Author, b1Pages, b1Read, b1bookNum))
+myLibrary.push(new book(b2Name, b2Author, b2Pages, b2Read, b2bookNum))
+myLibrary.push(new book(b3Name, b3Author, b3Pages, b3Read, b3bookNum))
+myLibrary.push(new book(b4Name, b4Author, b4Pages, b4Read, b4bookNum))
+myLibrary.push(new book(b5Name, b5Author, b5Pages, b5Read, b5bookNum))
 
 document.getElementById('submit').addEventListener('click', () => addBookToLibrary(addToDisplayBooks));
 
-const bookstitle = document.getElementById("title")
+//const bookstitle = document.getElementById("title")
 
 function addBookToLibrary(callback) {
+    let Num = myLibrary.length;
     const name = document.getElementById('name').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
+    const read = "Have Not Read"
+    const bookNum = "book"+Num;
 
-    myLibrary.push(new book(name, author, pages))
+    myLibrary.push(new book(name, author, pages, read, bookNum))
 
     return callback();
 }
@@ -62,6 +78,7 @@ function addBookHTML(libraryNum) {
     bookid = myLibrary[libraryNum];
     const newBook = document.createElement("div");
     newBook.className = "books"
+    newBook.setAttribute("id", "bookCont"+libraryNum);
     container.appendChild(newBook);
 
     const Title = document.createElement('div');
@@ -79,26 +96,47 @@ function addBookHTML(libraryNum) {
     Pages.className = "pages";
     newBook.appendChild(Pages);
 
+    const statusContainer = document.createElement('div');
+    statusContainer.className = "statusContainer";
+    newBook.appendChild(statusContainer);
+
     const toggleSwitch = document.createElement('label');
     toggleSwitch.className = "switch";
-    newBook.appendChild(toggleSwitch);
+    statusContainer.appendChild(toggleSwitch);
 
     const checkBox = document.createElement('input');
     checkBox.className = "checked"
     checkBox.setAttribute("type", "checkbox");
-    checkBox.setAttribute("id", "book"+libraryNum);
+    checkBox.setAttribute("id", "selBook"+libraryNum);
     toggleSwitch.appendChild(checkBox);
-    
-    const bookId = document.querySelector('#book'+libraryNum)
-    bookId.addEventListener('click', myLibrary[libraryNum].haveRead())
 
     const slider = document.createElement('span');
     slider.className = "slider round";
     toggleSwitch.appendChild(slider);
 
-    //document.querySelector(bookid).addEventListener('change', myLibrary[bookid].haveRead());
-    //myLibrary[libraryNum].prototype = Object.create(book.prototype);
-    //document.querySelector('#book'+libraryNum).addEventListener('change', myLibrary[libraryNum].haveRead());
+    const readStatus = document.createElement('div');
+    readStatus.className = "readStatus";
+    readStatus.setAttribute("id", "book"+libraryNum);
+    readStatus.textContent = myLibrary[libraryNum].read;
+    statusContainer.appendChild(readStatus);
+
+    const remove = document.createElement('div');
+    remove.innerHTML = "REMOVE"
+    remove.className = "remove";
+    remove.setAttribute("id", "remove"+libraryNum);
+    newBook.appendChild(remove);
+
+    const removeBook = document.getElementById("remove"+libraryNum);
+    removeBook.addEventListener('click', () => {deleteBook(libraryNum)});
+
+    const selector = document.querySelector('#selBook'+libraryNum);
+    selector.addEventListener('change', () => {myLibrary[libraryNum].haveRead()});
+}
+
+function deleteBook(num){
+    const book = document.getElementById("bookCont"+num)
+    book.remove()
+    delete myLibrary[num];
 }
 
 function displayBooks() {
@@ -112,10 +150,9 @@ function displayBooks() {
     }
 }
 function addToDisplayBooks() {
-    let myLibraryEnd = myLibrary.length - 1
+    let myLibraryEnd = myLibrary.length - 1;
     addBookHTML(myLibraryEnd);
 }
 
 
 displayBooks();
-//document.querySelector('.checked').addEventListener('change', myLibrary.haveRead());
